aa7c09ee9ce3ab2cca66742c579e80162a31d65a
ViktoriaGallikova <viktoria.gallikova@gmail.com>
2020-02-21 18:25:02 +0100

    pridanie reset-css, axios, fetchData, zobrazenie pokemonov, loading stav

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ Během tvorby aplikace commituj jako by šlo o reálný projekt. Pošli odkaz na
 - pre tvorbu web alplikacie som pouzila [creat react app](https://github.com/facebook/create-react-app)
 - na tvorbu stylov som pouzila balicek [node-sass](https://www.npmjs.com/package/node-sass)
 - aby som nemusela vela stylovat tak som pouzila framework [React-Bootstrap](https://react-bootstrap.github.io/)
+- na stahovanie dat som pouzila balicek [axios](https://www.npmjs.com/package/axios)
+- na vyresetovanie defaultnych stylov som pouzila [reset-css](https://www.npmjs.com/package/reset-css)
--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
-import { Container, Table } from "react-bootstrap";
+import { Container, Table, Spinner, Row, Col } from "react-bootstrap";
+import axios from "axios";
+
+const fetchData = async url => await axios.get(url);
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData("https://pokeapi.co/api/v2/pokemon?limit=964").then(res => {
+      setData(res.data.results);
+    });
+  }, []);
+
+  if (data === null) {
+    return (
+      <Container className="h-100">
+        <Row className="text-center align-items-center h-100">
+          <Col>
+            <Spinner animation="border" />
+            <h2 className="pt-4">Loading...</h2>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+
   return (
     <Container>
       <Table striped bordered hover variant="dark">
@@ -13,10 +37,14 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>bulbasaur</td>
-          </tr>
+          {data.map((pokemon, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{pokemon.name}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </Container>
